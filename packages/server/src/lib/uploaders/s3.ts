33d1d1972fa0382dca94626844a9d69172de27c1
luke-h1 <luke.howsam@yahoo.com>
2021-07-04 10:57:04 +0100

    rm server comments

--- packages/server/src/lib/uploaders/s3.ts
+++ packages/server/src/lib/uploaders/s3.ts
@@ -1,11 +1,9 @@
-// store each image in it's own unique folder to avoid name duplicates
 import { v4 } from 'uuid';
 
 const AWS = require('aws-sdk');
 
-// load config data from .env file
 require('dotenv').config();
-// update AWS config env data
+
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_KEY,
@@ -14,11 +12,9 @@ AWS.config.update({
 });
 const s3 = new AWS.S3({ region: process.env.AWS_BUCKET_REGION });
 
-// my default params for s3 upload
-// I have a max upload size of 1 MB
 const s3DefaultParams = {
   ACL: 'public-read',
-  Bucket: process.env.S3_BUCKET_NAME,
+  Bucket: process.env.AWS_BUCKET_NAME,
   Conditions: [
     ['content-length-range', 0, 3096000], // 3 Mb
     { acl: 'public-read' },
@@ -26,7 +22,6 @@ const s3DefaultParams = {
 };
 
 // the actual upload happens here
-// @ts-ignore
 export const handleFileUpload = async (file) => {
   const { createReadStream, filename } = await file;
 
